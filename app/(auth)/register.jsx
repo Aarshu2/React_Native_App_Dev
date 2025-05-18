ebd215811b61cf5489d2d69d4e1af98f1b03fefc
Aarsh Bajaj <aarshbajaj22@gmail.com>
2025-05-18 12:43:29 -0400

    Buttons

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,13 +1,20 @@
- import { StyleSheet } from 'react-native'
+ import { StyleSheet, Text } from 'react-native'
 import { Link } from 'expo-router'
+
 
 // themed components
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
+import ThemedButton from '../../components/ThemedButton'
 import Spacer from '../../components/Spacer'
 
 
 const Register = () => {
+
+  const handleSubmit = () => {
+    console.log('register form submitted')
+  }
+
   return (
     <ThemedView style={styles.container}>
 
@@ -16,8 +23,12 @@ const Register = () => {
             Register For an Account
         </ThemedText>
 
-        <Spacer height={100}/>
+        <ThemedButton onpress={handleSubmit}> 
+          <Text style={{color: 'f2f2f2'}}>Register</Text>
+        </ThemedButton>
 
+        <Spacer height={100}/>
+ 
         <Link href='/login'>
             <ThemedText style={{textAlign: 'center'}}>
                 Login instead
@@ -33,7 +44,8 @@ export default Register
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "center", 
+    alignItems: "center"
   },
   title: {
     textAlign: "center",
